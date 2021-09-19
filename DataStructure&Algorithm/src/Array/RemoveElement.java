86e1f82f1e53acb34c7aef2a0f2773851df62143
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-09-19 14:38:05 +0700

    remove-element update

--- DataStructure&Algorithm/src/Array/RemoveElement.java
+++ DataStructure&Algorithm/src/Array/RemoveElement.java
@@ -14,11 +14,11 @@ public class RemoveElement {
 		}
 	}
 
-	public static int removeElement(int[] nums, int val) {
+	public static int removeElement(int[] nums, int a) {
 		int k = 0;
 		
 		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != val) {
+			if (nums[i] != a) {
 				nums[k] = nums[i];
 				k++;
 			}
