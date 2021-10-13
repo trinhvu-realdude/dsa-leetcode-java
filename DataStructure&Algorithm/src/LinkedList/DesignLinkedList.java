5c54b5ba30e96dbb575268872e50306a54197768
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-10-14 00:24:15 +0700

    complete DesignLinkedList

--- DataStructure&Algorithm/src/LinkedList/DesignLinkedList.java
+++ DataStructure&Algorithm/src/LinkedList/DesignLinkedList.java
@@ -2,105 +2,88 @@ package LinkedList;
 
 public class DesignLinkedList {
 
-	static class Node {
+	public static class Node {
 		int val;
 		Node next;
 
-		public Node(int x) {
-			this.val = x;
+		public Node(int val) {
+			this.val = val;
 		}
 	}
 
 	static class MyLinkedList {
-
 		private Node head;
 
 		public MyLinkedList() {
 			head = null;
 		}
 
-		/**
-		 * Get the value of the index-th node in the linked list. If the index is
-		 * invalid, return -1.
-		 */
 		public int get(int index) {
-			Node temp = head;
+			Node curNode = head;
 			int count = 0;
 
-			while (temp != null) {
+			while (curNode != null) {
 				if (count == index) {
-					return temp.val;
+					return curNode.val;
 				}
 				count++;
-				temp = temp.next;
+				curNode = curNode.next;
 			}
 			return -1;
 		}
 
-		/**
-		 * Add a node of value val before the first element of the linked list. After
-		 * the insertion, the new node will be the first node of the linked list.
-		 */
-		public Node addAtHead(int val, Node head) {
-			Node temp = new Node(val);
-
-			if (head != null) {
-				temp.next = head;
-			}
-
-			return temp;
+		public void addAtHead(int val) {
+			Node newNode = new Node(val);
+			newNode.next = head;
+			head = newNode;
 		}
 
-		/** Append a node of value val to the last element of the linked list. */
-		public Node addAtTail(int val, Node head) {
+		public void addAtTail(int val) {
 			Node lastNode = head;
 			Node newNode = new Node(val);
 
 			if (head != null) {
-				while (lastNode.next != null) {
+				while (lastNode != null) {
+					if (lastNode.next == null) {
+						lastNode.next = newNode;
+						newNode.next = null;
+						return;
+					}
 					lastNode = lastNode.next;
 				}
-				lastNode.next = newNode;
-				return head;
 			}
-
-			return newNode;
+			addAtHead(val);
 		}
 
-		/**
-		 * Add a node of value val before the index-th node in the linked list. If index
-		 * equals to the length of linked list, the node will be appended to the end of
-		 * linked list. If index is greater than the length, the node will not be
-		 * inserted.
-		 */
-		public Node addAtIndex(int index, int val, Node head) {
-			Node curNode = head;
+		public void addAtIndex(int index, int val) {
 			Node newNode = new Node(val);
+			Node curNode = head;
 			int count = 0;
 
 			if (index == 0) {
-				return addAtHead(val, head);
+				addAtHead(val);
 			} else {
 				while (curNode != null) {
 					count++;
-
 					if (count == index) {
 						newNode.next = curNode.next;
 						curNode.next = newNode;
-						break;
+						return;
 					}
 					curNode = curNode.next;
 				}
-				return head;
+
+				if (count == index) {
+					addAtTail(val);
+				}
 			}
 		}
 
-		/** Delete the index-th node in the linked list, if the index is valid. */
-		public Node deleteAtIndex(int index, Node head) {
+		public void deleteAtIndex(int index) {
 			Node curNode = head;
 			Node prevNode = null;
 			int count = 0;
-			
+
 			if (index != 0) {
 				while (curNode != null) {
 					if (count == index) {
@@ -110,18 +93,16 @@ public class DesignLinkedList {
 					prevNode = curNode;
 					curNode = curNode.next;
 				}
-				return head;
+			} else {
+				head = head.next;
 			}
-			return curNode.next;
 		}
 
-		/** Print the Linked List */
-		public void printLinkedList(Node head) {
+		public void printLinkedList() {
 			if (head == null) {
-				System.out.println("List is empty");
+				System.out.println("List is empty.");
 			} else {
 				Node temp = head;
-
 				while (temp != null) {
 					System.out.print(temp.val);
 					temp = temp.next;
@@ -137,37 +118,22 @@ public class DesignLinkedList {
 	}
 
 	public static void main(String[] args) {
-		Node n1 = new Node(1);
-		Node n2 = new Node(2);
-		Node n3 = new Node(3);
-
-		n1.next = n2;
-		n2.next = n3;
-
-		MyLinkedList myList = new MyLinkedList();
-
-		System.out.print("Linked List: ");
-		myList.printLinkedList(n1);
-
-//		Add at head
-		n1 = myList.addAtHead(0, n1);
-		System.out.print("New Head: ");
-		myList.printLinkedList(n1);
-
-//		Add at tail
-		n1 = myList.addAtTail(4, n1);
-		System.out.print("New Tail: ");
-		myList.printLinkedList(n1);
-
-//		Add at index
-		n1 = myList.addAtIndex(1, 5, n1);
-		System.out.print("New Index: ");
-		myList.printLinkedList(n1);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.addAtHead(1);
+		myLinkedList.printLinkedList();
 		
-//		Delete at index
-		n1 = myList.deleteAtIndex(3, n1);
-		System.out.print("Delete Index: ");
-		myList.printLinkedList(n1);
+		myLinkedList.addAtTail(3);
+		myLinkedList.printLinkedList();
+		
+		myLinkedList.addAtIndex(1, 2);
+		myLinkedList.printLinkedList();
+		
+		System.out.println(myLinkedList.get(1)); // return 2
+		
+		myLinkedList.deleteAtIndex(1); // now the linked list is 1->3
+		myLinkedList.printLinkedList();
+		
+		System.out.println(myLinkedList.get(1)); // return 3
 	}
 
 }
